34f3e7f825d427879f4d1a3b46302e6548fb1d68
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-07-09 11:27:03 +0530

    add-selector

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ let customerList = [];
 
 function addCustomer(){
     let cusName = document.getElementById("name").value;
+    let cusTitle = document.getElementById("title").value;
     let cusAdd = document.getElementById("address").value;
     let cusSalary = document.getElementById("salary").value;
     let cusPass = document.getElementById("password").value;
     
     let newCus = {
         name: cusName,
+        title: cusTitle,
         address: cusAdd,
         salary: cusSalary,
         password: cusPass
@@ -34,6 +36,7 @@ function loadTable(newCustomerRef){
     cusTable.innerHTML += `
         <tr>
             <td>${newCustomerRef.name}</td>
+            <td>${newCustomerRef.title}</td>
             <td>${newCustomerRef.address}</td>
             <td>${newCustomerRef.salary}</td>
             <td>${newCustomerRef.password}</td>
