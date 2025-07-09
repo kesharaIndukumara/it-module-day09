a15c5313551cff3f9cd9843dcdb9df70d77a7ab7
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-07-09 11:59:54 +0530

    endOfLastDayExample

--- app.js
+++ app.js
@@ -19,15 +19,29 @@ function addCustomer(){
 
     customerList.push(newCus);
 
+    // customerList.push(
+    //     {
+    //         name:cusName,
+    //         title:cusTitle,
+    //         address: cusAdd,
+    //         salary: cusSalary,
+    //         password: cusPass
+    //     }
+    // );
+
     console.log(customerList);
 
     document.getElementById("name").value = "";
     document.getElementById("address").value = "";
     document.getElementById("salary").value = "";
     document.getElementById("password").value = "";
+    document.getElementById("title").value = "";
 
 
     loadTable(newCus);
+    // loadTable();
+
+    console.log(document.getElementById("btnReg"))
 }
 
 function loadTable(newCustomerRef){
@@ -36,10 +50,26 @@ function loadTable(newCustomerRef){
     cusTable.innerHTML += `
         <tr>
             <td>${newCustomerRef.name}</td>
-            <td>${newCustomerRef.title}</td>
+            <td>${newCustomerRef.title}.${newCustomerRef.name}</td>
             <td>${newCustomerRef.address}</td>
             <td>${newCustomerRef.salary}</td>
             <td>${newCustomerRef.password}</td>
         </tr>
     `
+}
+
+function loadTable(){
+    let cusTable = document.getElementById("cusTable");
+
+    customerList.forEach(customer =>{
+        cusTable.innerHTML += `
+            <tr>
+                <td>${customer.name}</td>
+                <td>${customer.title}.${customer.name}</td>
+                <td>${customer.address}</td>
+                <td>${customer.salary}</td>
+                <td>${customer.password}</td>
+            </tr>
+        `
+    })
 }
